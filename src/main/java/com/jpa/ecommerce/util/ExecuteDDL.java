0a4040ef5d61cf930792886b04a2492fe41f8a82
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-16 12:51:56 -0300

    7.16 | Configuring unit persistence properties dynamically for schema generation

---
+++ src/main/java/com/jpa/ecommerce/util/ExecuteDDL.java
@@ -0,0 +1,36 @@
+package com.jpa.ecommerce.util;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExecuteDDL {
+
+    //To execute this class, you must comment in the persistence.xml what is between the two <!--**-->
+    public static void main(String[] args) {
+        Map<String, String> properties = new HashMap<>();
+
+        properties.put("javax.persistence.schema-generation.database.action",
+                "drop-and-create");
+
+        properties.put("javax.persistence.schema-generation.create-source",
+                "metadata-then-script");
+        properties.put("javax.persistence.schema-generation.drop-source",
+                "metadata-then-script");
+
+        properties.put("javax.persistence.schema-generation.create-script-source",
+                "META-INF/database/script-create.sql");
+        properties.put("javax.persistence.schema-generation.drop-script-source",
+                "META-INF/database/script-remove.sql");
+
+        properties.put("javax.persistence.sql-load-script-source",
+                "META-INF/database/initial-data.sql");
+
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU", properties);
+
+        entityManagerFactory.close();
+    }
+}
