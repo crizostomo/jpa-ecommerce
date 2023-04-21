403737b4dc080fafa34982558d90aaeb4c1bcccf
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-21 10:23:09 -0300

    9.2 | Differences between TypedQuery and Query

--- src/test/java/com/jpa/ecommerce/jpql/BasicJPQLTest.java
+++ src/test/java/com/jpa/ecommerce/jpql/BasicJPQLTest.java
@@ -2,6 +2,7 @@ package com.jpa.ecommerce.jpql;
 
 import com.jpa.ecommerce.EntityManagerTest;
 import com.jpa.ecommerce.model.Order;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,5 +21,18 @@ public class BasicJPQLTest extends EntityManagerTest {
 
         List<Order> list = typedQuery.getResultList();
         Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void showQueriesDifference() {
+        String jpql = "select o from Order o where o.id = 1";
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery(jpql, Order.class);
+        Order orderTypedQuery = typedQuery.getSingleResult();
+        Assert.assertNotNull(orderTypedQuery);
+
+        Query query = entityManager.createQuery(jpql);
+        Order orderQuery = (Order) query.getSingleResult();
+        Assert.assertNotNull(orderQuery);
     }
 }
