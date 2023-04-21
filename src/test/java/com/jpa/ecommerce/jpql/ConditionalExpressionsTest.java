2b22f393c141c5471c6b8bc01651e874e4c6bdf3
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-21 20:06:20 -0300

    9.13 | Using conditional expressions 'is null' and 'is empty'

--- src/test/java/com/jpa/ecommerce/jpql/ConditionalExpressionsTest.java
+++ src/test/java/com/jpa/ecommerce/jpql/ConditionalExpressionsTest.java
@@ -10,6 +10,27 @@ import java.util.List;
 public class ConditionalExpressionsTest extends EntityManagerTest {
 
     @Test
+    public void isNull() {
+        String jpql = "select p from Product p where p.photo is null";
+//        String jpql = "select p from Product p where p.photo is not null";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void isEmpty() {
+        String jpql = "select p from Product p where p.categories is empty";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void useConditionalExpressionLike() {
         String jpql = "select c from Client c where c.name like concat(:name, '%')";
 //        String jpql = "select c from Client c where c.name like concat('%', :name, '%')"; // You want to search the middle
