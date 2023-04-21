9696d5a0542f6d4de4777d8045f3c768a2dd5e67
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-21 10:32:28 -0300

    9.3 | Selecting an entity attribute as return

--- src/test/java/com/jpa/ecommerce/jpql/BasicJPQLTest.java
+++ src/test/java/com/jpa/ecommerce/jpql/BasicJPQLTest.java
@@ -1,7 +1,9 @@
 package com.jpa.ecommerce.jpql;
 
 import com.jpa.ecommerce.EntityManagerTest;
+import com.jpa.ecommerce.model.Client;
 import com.jpa.ecommerce.model.Order;
+import com.jpa.ecommerce.model.Product;
 import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import org.junit.Assert;
@@ -10,6 +12,20 @@ import org.junit.Test;
 import java.util.List;
 
 public class BasicJPQLTest extends EntityManagerTest {
+
+    @Test
+    public void selectAnAttributeToReturn() {
+        String jpql = "select p.name from Product p where p.id =1";
+
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> list = typedQuery.getResultList();
+        Assert.assertTrue(String.class.equals(list.get(0).getClass()));
+
+        String clientJpql = "select o.client from Order o";
+        TypedQuery<Client> clientTypedQuery = entityManager.createQuery(clientJpql, Client.class);
+        List<Client> clientList = clientTypedQuery.getResultList();
+        Assert.assertTrue(Client.class.equals(clientList.get(0).getClass()));
+    }
 
     @Test
     public void searchByIdentifier() {
