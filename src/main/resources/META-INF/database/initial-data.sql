bc7997d7c9b0d37d3400af1b9d88892949114af0
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-21 12:46:32 -0300

    9.6 | Using inner join among entities

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -1,5 +1,5 @@
-insert into product (id, name, price, creation_date, description) values (1, 'Kindle', 499.0, date_sub(sysdate(), interval 1 day), 'Check it out the new Kindle.');
-insert into product (id, name, price, creation_date, description) values (3, 'Kindle II', 699.0, date_sub(sysdate(), interval 1 day), 'Check it out the new Kindle II.');
+insert into product (id, name, price, creation_date, description) values (1, 'Kindle', 500.0, date_sub(sysdate(), interval 1 day), 'Check it out the new Kindle.');
+insert into product (id, name, price, creation_date, description) values (3, 'Kindle II', 700.0, date_sub(sysdate(), interval 1 day), 'Check it out the new Kindle II.');
 
 insert into client (id, name, cpf) values (1, 'Roronoa Zoro', '000');
 insert into client (id, name, cpf) values (2, 'Monkey D. Luffy', '333');
@@ -7,11 +7,12 @@ insert into client (id, name, cpf) values (2, 'Monkey D. Luffy', '333');
 insert into client_detail (client_id, gender, birth_date) values (1, 'MALE', date_sub(sysdate(), interval 27 year));
 insert into client_detail (client_id, gender, birth_date) values (2, 'MALE', date_sub(sysdate(), interval 30 year));
 
-insert into `order` (id, client_id, creation_date, total, status) values (1, 1, sysdate(), 998.0, 'WAITING');
-insert into `order` (id, client_id, creation_date, total, status) values (2, 1, sysdate(), 499.0, 'WAITING');
+insert into `order` (id, client_id, creation_date, total, status) values (1, 1, sysdate(), 1700.0, 'WAITING');
+insert into `order` (id, client_id, creation_date, total, status) values (2, 1, sysdate(), 500.0, 'WAITING');
 
-insert into order_item (order_id, product_id, product_price, quantity) values (1, 1, 499, 2);
-insert into order_item (order_id, product_id, product_price, quantity) values (2, 1, 499, 1);
+insert into order_item (order_id, product_id, product_price, quantity) values (1, 1, 500, 2);
+insert into order_item (order_id, product_id, product_price, quantity) values (1, 3, 700, 1);
+insert into order_item (order_id, product_id, product_price, quantity) values (2, 1, 500, 1);
 
 insert into category (id, name) values (1, 'Electronics Products');
 insert into category (id, name) values (2, 'Sci-Fi Books');
---
+++ src/test/java/com/jpa/ecommerce/jpql/JoinTest.java
@@ -0,0 +1,43 @@
+package com.jpa.ecommerce.jpql;
+
+import com.jpa.ecommerce.EntityManagerTest;
+import com.jpa.ecommerce.model.Order;
+import jakarta.persistence.TypedQuery;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void usingJoin() {
+        String jpql = "select o from Order o inner join o.payment pay";
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery(jpql, Order.class);
+        List<Order> list = typedQuery.getResultList();
+        Assert.assertTrue(list.size() == 1);
+    }
+
+
+    @Test
+    public void usingJoinWithArray() {
+        String jpql = "select o, pay from Order o inner join o.payment pay";
+//        String jpql = "select o, i from Order o inner join o.orderItems i";
+//        String jpql = "select o from Order o inner join o.orderItems i where i.productPrice > 10";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> list = typedQuery.getResultList();
+//        Assert.assertTrue(list.size() == 3); // using orderItems
+        Assert.assertTrue(list.size() == 1);
+    }
+
+    @Test
+    public void usingJoinWithArrayAndFilter() {
+        String jpql = "select o, pay from Order o inner join o.payment pay where pay.status = 'PROCESSING'";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertTrue(list.size() == 1);
+    }
+}
