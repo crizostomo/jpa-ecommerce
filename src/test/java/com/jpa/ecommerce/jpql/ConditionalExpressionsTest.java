c5b2d7f1340d5defb4bf5ce23500b7b57d813bb1
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-21 20:00:08 -0300

    9.12 | Using conditional expression like

---
+++ src/test/java/com/jpa/ecommerce/jpql/ConditionalExpressionsTest.java
@@ -0,0 +1,24 @@
+package com.jpa.ecommerce.jpql;
+
+import com.jpa.ecommerce.EntityManagerTest;
+import jakarta.persistence.TypedQuery;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ConditionalExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void useConditionalExpressionLike() {
+        String jpql = "select c from Client c where c.name like concat(:name, '%')";
+//        String jpql = "select c from Client c where c.name like concat('%', :name, '%')"; // You want to search the middle
+//        String jpql = "select c from Client c where c.name like concat('%', :name)"; // If you want what it is at the end
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        typedQuery.setParameter("name", "Roronoa");
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+}
