0c212934860db858b3ff8ceb0cd75a51277f40d8
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-21 14:57:48 -0300

    9.10 | Consulting orders with a specific product

--- src/test/java/com/jpa/ecommerce/jpql/PathExpressionTest.java
+++ src/test/java/com/jpa/ecommerce/jpql/PathExpressionTest.java
@@ -1,6 +1,7 @@
 package com.jpa.ecommerce.jpql;
 
 import com.jpa.ecommerce.EntityManagerTest;
+import com.jpa.ecommerce.model.Order;
 import jakarta.persistence.TypedQuery;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +9,18 @@ import org.junit.Test;
 import java.util.List;
 
 public class PathExpressionTest extends EntityManagerTest {
+
+    @Test
+    public void searchOrdersWithASpecificProduct() {
+        String jpql = "select o from Order o join o.orderItems i where i.id.productId = 1";
+//        String jpql = "select o from Order o join o.orderItems i where i.product.id = 1";
+//        String jpql = "select o from Order o join o.orderItems i join i.product pro where pro.id = 1";
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery(jpql, Order.class);
+
+        List<Order> list = typedQuery.getResultList();
+        Assert.assertTrue(list.size() == 2);
+    }
 
     @Test
     public void usePathExpressions() {
