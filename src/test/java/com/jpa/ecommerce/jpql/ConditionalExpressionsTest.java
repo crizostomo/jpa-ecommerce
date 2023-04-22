6b04cff37fc8e2a734d10e39877624e5fe43a55f
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-22 13:24:52 -0300

    9.16 | Conditional expression between

--- src/test/java/com/jpa/ecommerce/jpql/ConditionalExpressionsTest.java
+++ src/test/java/com/jpa/ecommerce/jpql/ConditionalExpressionsTest.java
@@ -14,6 +14,23 @@ import java.util.List;
 public class ConditionalExpressionsTest extends EntityManagerTest {
 
     @Test
+    public void useOfBetween() {
+//        String jpql = "select p from Product p where p.price between :initialPrice and :finalPrice";
+        String jpql = "select o from Order o where o.creationDate between :initialDate and :finalDate";
+
+//        TypedQuery<Product> typedQuery = entityManager.createQuery(jpql, Product.class);
+//        typedQuery.setParameter("initialPrice", new BigDecimal(400));
+//        typedQuery.setParameter("finalPrice", new BigDecimal(1500));
+        TypedQuery<Order> typedQuery = entityManager.createQuery(jpql, Order.class);
+        typedQuery.setParameter("initialDate", LocalDateTime.now().minusDays(2));
+        typedQuery.setParameter("finalDate", LocalDateTime.now());
+
+//        List<Product> list = typedQuery.getResultList();
+        List<Order> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void greaterLesserWithDates() {
         String jpql = "select o from Order o where o.creationDate > :date";
 
