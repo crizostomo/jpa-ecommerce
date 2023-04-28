f934d57cecfb54b20e27b5779c07190772c2119a
crizostomo <diogo_crizostomo@hotmail.com>
2023-04-27 23:27:33 -0300

    9.30 | Using the where clause with group by

--- src/test/java/com/jpa/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/jpa/ecommerce/jpql/GroupByTest.java
@@ -1,6 +1,7 @@
 package com.jpa.ecommerce.jpql;
 
 import com.jpa.ecommerce.EntityManagerTest;
+import com.jpa.ecommerce.model.OrderStatus;
 import jakarta.persistence.TypedQuery;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +9,36 @@ import org.junit.Test;
 import java.util.List;
 
 public class GroupByTest extends EntityManagerTest {
+
+    @Test
+    public void groupAndFilterResult() {
+//         Total amount of sales per month
+//        String jpql = "select concat(year(o.creationDate), '/', function('monthname', o.creationDate)), sum(o.total) " +
+//                "from Order o " +
+//                "where year(o.creationDate) = year(current_date) " +
+//                "and o.status = :status " +
+//                "group by year(o.creationDate), month(o.creationDate), o.creationDate";
+
+//         Total amount of sales per category
+//        String jpql = "select c.name, sum(oi.productPrice) from OrderItem oi " +
+//                "join oi.product pro join pro.categories c join oi.order o " +
+//                "where year(o.creationDate) = year(current_date) and month(o.creationDate) = month(current_date) " +
+//                "group by c.id";
+
+//                Total amount of sales by client
+        String jpql = "select c.name, sum(oi.productPrice) from OrderItem oi " +
+                "join oi.order o join o.client c join oi.order o2 " +
+                "where year(o.creationDate) = year(current_date) and month(o.creationDate) >= (month(current_date) - 3) " +
+                "group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+//        typedQuery.setParameter("status", OrderStatus.PAID);
+
+        List<Object[]> list = typedQuery.getResultList();
+        Assert.assertFalse(list.isEmpty());
+
+        list.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
 
     @Test
     public void groupResult() {
