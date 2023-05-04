8bbc0bdf9ed46cdc7a912ede3e5f568300d9a8ad
crizostomo <diogo_crizostomo@hotmail.com>
2023-05-04 16:20:26 -0300

    10.7 | Inner join among entities

---
+++ src/test/java/com/jpa/ecommerce/criteria/JoinCriteriaTest.java
@@ -0,0 +1,47 @@
+package com.jpa.ecommerce.criteria;
+
+import com.jpa.ecommerce.EntityManagerTest;
+import com.jpa.ecommerce.model.*;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void usingJoinInOrder() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Order> criteriaQuery = criteriaBuilder.createQuery(Order.class);
+        Root<Order> root = criteriaQuery.from(Order.class);
+        Join<Order, Payment> joinPayment = root.join("payment"); // Equivalent to "select o from Order o join o.payment pay"
+//        Join<Order, OrderItem> joinItems = root.join("orderItems");
+//        Join<OrderItem, Product> joinItemProduct = joinItems.join("product");
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Order> list = typedQuery.getResultList();
+        Assert.assertTrue(list.size() == 4);
+    }
+
+    @Test
+    public void usingJoinInPayment() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Payment> criteriaQuery = criteriaBuilder.createQuery(Payment.class);
+        Root<Order> root = criteriaQuery.from(Order.class);
+        Join<Order, Payment> joinPayment = root.join("payment"); // Equivalent to "select o from Order o join o.payment pay"
+
+        criteriaQuery.select(joinPayment);
+        criteriaQuery.where(criteriaBuilder.equal(joinPayment.get("status"), PaymentStatus.PROCESSING));
+
+        TypedQuery<Payment> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Payment> list = typedQuery.getResultList();
+        Assert.assertTrue(list.size() == 2);
+    }
+}
