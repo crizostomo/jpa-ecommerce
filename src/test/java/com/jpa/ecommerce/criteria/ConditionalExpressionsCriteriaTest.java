3c4d301fd6476ecab73c819f0a7994162bd0f966
crizostomo <diogo_crizostomo@hotmail.com>
2023-05-11 11:25:54 -0300

    10.15 | Using conditional expressions is null and is empty

--- src/test/java/com/jpa/ecommerce/criteria/ConditionalExpressionsCriteriaTest.java
+++ src/test/java/com/jpa/ecommerce/criteria/ConditionalExpressionsCriteriaTest.java
@@ -4,6 +4,7 @@ import com.jpa.ecommerce.EntityManagerTest;
 import com.jpa.ecommerce.model.Client;
 import com.jpa.ecommerce.model.Client_;
 import com.jpa.ecommerce.model.Product;
+import com.jpa.ecommerce.model.Product_;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -15,6 +16,37 @@ import java.util.List;
 
 public class ConditionalExpressionsCriteriaTest extends EntityManagerTest {
 
+    @Test // Similar to the exercise in the Class ConditionalExpressionsTest - isEmpty
+    public void isEmpty() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        criteriaQuery.select(root);
+
+//        criteriaQuery.where(root.get(Product_.categories).isEmpty()); // Option 1
+        criteriaQuery.where(criteriaBuilder.isEmpty(root.get(Product_.categories))); // Option 2 | You could use isNotEmpty too
+
+        TypedQuery<Product> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Product> productList = typedQuery.getResultList();
+        Assert.assertTrue(productList.isEmpty());
+    }
+
+    @Test // Similar to the exercise in the Class ConditionalExpressionsTest - isNull
+    public void isNull() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        criteriaQuery.select(root);
+
+//        criteriaQuery.where(root.get(Product_.photo).isNull()); // Option 1
+        criteriaQuery.where(criteriaBuilder.isNull(root.get(Product_.photo))); // Option 2 | You could use isNotNull too
+
+        TypedQuery<Product> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Product> productList = typedQuery.getResultList();
+        Assert.assertFalse(productList.isEmpty());
+    }
 
     @Test  // Similar to the exercise in the Class ConditionalExpressionsTest - useConditionalExpressionLike
     public void conditionalExpressionLike() {
