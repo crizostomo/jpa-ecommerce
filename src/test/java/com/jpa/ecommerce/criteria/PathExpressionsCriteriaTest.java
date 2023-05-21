5f7baf1bd5005074c0fcaf1e6a13675aa4b6b71d
crizostomo <diogo_crizostomo@hotmail.com>
2023-05-21 12:59:17 -0300

    10.32 | Consulting orders with a specific product

--- src/test/java/com/jpa/ecommerce/criteria/PathExpressionsCriteriaTest.java
+++ src/test/java/com/jpa/ecommerce/criteria/PathExpressionsCriteriaTest.java
@@ -1,12 +1,11 @@
 package com.jpa.ecommerce.criteria;
 
 import com.jpa.ecommerce.EntityManagerTest;
-import com.jpa.ecommerce.model.Client_;
-import com.jpa.ecommerce.model.Order;
-import com.jpa.ecommerce.model.Order_;
+import com.jpa.ecommerce.model.*;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Root;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,6 +13,23 @@ import org.junit.Test;
 import java.util.List;
 
 public class PathExpressionsCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void searchOrdersWithProductExercise() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Order> criteriaQuery = criteriaBuilder.createQuery(Order.class);
+        Root<Order> root = criteriaQuery.from(Order.class);
+        Join<Order, OrderItem> orderOrderItemJoin = root.join(Order_.orderItems);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.equal(orderOrderItemJoin.get(OrderItem_.product).get(Product_.id), 1));
+
+        TypedQuery<Order> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Order> list = typedQuery.getResultList();
+
+        Assert.assertFalse(list.isEmpty());
+    }
 
     @Test
     public void usingPathExpression() {
