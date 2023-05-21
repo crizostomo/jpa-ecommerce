8bb5c12b45b30cf75e200691bcf121f29b220952
crizostomo <diogo_crizostomo@hotmail.com>
2023-05-21 13:11:48 -0300

    10.33 | Grouping registers by using functions in group by

--- src/test/java/com/jpa/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/jpa/ecommerce/criteria/GroupByCriteriaTest.java
@@ -1,8 +1,8 @@
 package com.jpa.ecommerce.criteria;
 
 import com.jpa.ecommerce.EntityManagerTest;
-import com.jpa.ecommerce.model.*;
 import com.jpa.ecommerce.model.Order;
+import com.jpa.ecommerce.model.*;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import org.junit.Assert;
@@ -11,6 +11,39 @@ import org.junit.Test;
 import java.util.List;
 
 public class GroupByCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void groupingResultsByUsingFunctions() {
+//        Total amount of sales per month.
+//        String jpql = "select concat(year(o.creationDate), '/', function('monthname', o.creationDate)), sum(p.total) " +
+//                " from Order o " +
+//                " group by year(o.creationDate), month(o.creationDate) ";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Order> root = criteriaQuery.from(Order.class);
+
+        Expression<Integer> creationYearOrder = criteriaBuilder
+                .function("year", Integer.class, root.get(Order_.creationDate));
+        Expression<Integer> creationMonthOrder = criteriaBuilder
+                .function("month", Integer.class, root.get(Order_.creationDate));
+        Expression<String> creationMonthNameOrder = criteriaBuilder
+                .function("monthname", String.class, root.get(Order_.creationDate));
+
+        Expression<String> anoMesConcat = criteriaBuilder.concat(criteriaBuilder.concat(creationYearOrder.as(String.class), "/"), creationMonthNameOrder);
+
+        criteriaQuery.multiselect(
+                anoMesConcat,
+                criteriaBuilder.sum(root.get(Order_.total))
+        );
+
+        criteriaQuery.groupBy(creationYearOrder, creationMonthOrder);
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> list = typedQuery.getResultList();
+
+        list.forEach(arr -> System.out.println("Year/Month: " + arr[0] + ", Sum: " + arr[1]));
+    }
 
     @Test
     public void groupResultExercise() {
