5133b7f77e8d80a1a65a4f65ac687ff683f2b80e
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-05 18:56:24 -0300

    11.4 | Inserting parameters for a native search

--- src/test/java/com/jpa/ecommerce/nativeQuerySearchTest/NativeSearchTest.java
+++ src/test/java/com/jpa/ecommerce/nativeQuerySearchTest/NativeSearchTest.java
@@ -11,6 +11,19 @@ import java.util.List;
 public class NativeSearchTest extends EntityManagerTest {
 
     @Test
+    public void inputParameters() {
+        String sql = "select prd_id id, prd_name name, prd_description description, prd_creation_date creation_date, " +
+                     "prd_last_update_date prd_last_update_date, prd_price price, prd_photo photo from product_ecm where prd_id = :id";
+
+        Query query = entityManager.createNativeQuery(sql, Product.class);
+        query.setParameter("id", 201);
+
+        List<Product> list = query.getResultList();
+
+        list.stream().forEach(obj -> System.out.println(String.format("Product => ID: %s, Name: %s", obj.getId(), obj.getName())));
+    }
+
+    @Test
     public void executeSQLReturningEntity() {
         String sql = "select id, name, description, creation_date, last_update_date, price, photo from product";
 //        String sql = "select id, name, description, creation_date, last_update_date, price, photo from store_product";
