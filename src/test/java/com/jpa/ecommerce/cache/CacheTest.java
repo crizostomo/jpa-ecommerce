1fae3e504e10f52803be2cda71268a2f03b560a3
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-17 18:42:47 -0300

    13.3. | Removing entities from cache

--- src/test/java/com/jpa/ecommerce/cache/CacheTest.java
+++ src/test/java/com/jpa/ecommerce/cache/CacheTest.java
@@ -1,6 +1,7 @@
 package com.jpa.ecommerce.cache;
 
 import com.jpa.ecommerce.model.Order;
+import jakarta.persistence.Cache;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -45,6 +46,26 @@ public class CacheTest {
                 .getResultList();
 
         System.out.println("Searching from instance 2: ");
+        entityManager2.find(Order.class, 2);
+    }
+
+    @Test
+    public void removeFromCache() {
+        Cache cache = entityManagerFactory.getCache()
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+        System.out.println("Searching from instance 1: ");
+        entityManager1.createQuery("select o from Order o", Order.class)
+                .getResultList();
+
+        System.out.println("Removing order 1 from cache ");
+//        cache.evict(Order.class, 1); // To remove just order of id 1
+//        cache.evict(Order.class);
+        cache.evictAll(); // To remove everything in the cache
+
+        System.out.println("Searching from instance 2: ");
+        entityManager2.find(Order.class, 1);
         entityManager2.find(Order.class, 2);
     }
 }
