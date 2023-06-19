d97c4388cc06bf9b6e4cf46fe56911b7f0e58a30
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-19 19:54:56 -0300

    14.9 | Lock with JPQL and Criteria API

--- src/test/java/com/jpa/ecommerce/concurrency/PessimistLockTest.java
+++ src/test/java/com/jpa/ecommerce/concurrency/PessimistLockTest.java
@@ -10,6 +10,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.List;
+
 public class PessimistLockTest {
 
     protected static EntityManagerFactory entityManagerFactory;
@@ -35,6 +37,91 @@ public class PessimistLockTest {
         try {
             Thread.sleep(seconds * 1000);
         } catch (InterruptedException e) {}
+    }
+
+    //    @Test // To execute this test it is needed ti comment the version
+    public void usingLockInTypedQuery() {
+        Runnable runnable1 = () -> {
+            log("Starting Runnable 01.");
+
+            String newDescription = "Detailed description. CTM: " + System.currentTimeMillis();
+
+            EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+            entityManager1.getTransaction().begin();
+
+            log("Runnable 01 will load product 1.");
+
+            List<Product> list = entityManager1
+                    .createQuery("select p from Product p where p.id in (3,4,5)")
+                    .setLockMode(LockModeType.PESSIMISTIC_READ)
+                    .getResultList();
+
+            Product product = list
+                    .stream()
+                    .filter(p -> p.getId().equals(1))
+                    .findFirst()
+                    .get();
+
+            log("Runnable 01 will alter the product of ID equal to 1.");
+            product.setDescription(newDescription);
+
+            log("Runnable 01 will wait for 3 seconds.");
+            wait(3);
+
+            log("Runnable 01 will confirm the transaction.");
+            entityManager1.getTransaction().commit();
+            entityManager1.close();
+
+            log("Finishing Runnable 01.");
+        };
+
+        Runnable runnable2 = () -> {
+            log("Starting Runnable 02.");
+
+            String newDescription = "Detailed description 2 CTM: " + System.currentTimeMillis();
+
+            EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+            entityManager2.getTransaction().begin();
+
+            log("Runnable 02 will load product 2.");
+            Product product = entityManager2.find(
+                    Product.class, 1, LockModeType.PESSIMISTIC_WRITE);
+
+            log("Runnable 02 will alter the procut.");
+            product.setDescription(newDescription);
+
+            log("Runnable 02 will wait for 1 second.");
+            wait(1);
+
+            log("Runnable 02 will confirm the transaction.");
+            entityManager2.getTransaction().commit();
+            entityManager2.close();
+
+            log("Finishing Runnable 02.");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+
+        wait(1);
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        EntityManager entityManager3 = entityManagerFactory.createEntityManager();
+        Product product = entityManager3.find(Product.class, 1);
+        entityManager3.close();
+
+        Assert.assertTrue(product.getDescription().startsWith("Detailed description 2"));
+
+        log("Finishing test method.");
     }
 
     //    @Test // To execute this test it is needed ti comment the version
