9c26d010e05fb9ba58df3d4a79b2aebbd1e310ea
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-19 19:46:05 -0300

    14.7 | Understanding what happens if we mix more than one lock

--- src/test/java/com/jpa/ecommerce/concurrency/PessimistLockTest.java
+++ src/test/java/com/jpa/ecommerce/concurrency/PessimistLockTest.java
@@ -37,6 +37,84 @@ public class PessimistLockTest {
         } catch (InterruptedException e) {}
     }
 
+    //    @Test // To execute this test it is needed ti comment the version
+    public void mixingLockTypes() {
+        Runnable runnable1 = () -> {
+            log("Starting Runnable 01.");
+
+            String newDescription = "Detailed description. CTM: " + System.currentTimeMillis();
+
+            EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+            entityManager1.getTransaction().begin();
+
+            log("Runnable 01 will load product 1.");
+            Product product = entityManager1.find(
+                    Product.class, 1, LockModeType.PESSIMISTIC_WRITE);
+//                    Product.class, 1, LockModeType.PESSIMISTIC_READ);
+
+            log("Runnable 01 will alter the product.");
+            product.setDescription(newDescription);
+
+            log("Runnable 01 will wait for 3 seconds.");
+            wait(3);
+
+            log("Runnable 01 will confirm the transaction.");
+            entityManager1.getTransaction().commit();
+            entityManager1.close();
+
+            log("Finishing Runnable 01.");
+        };
+
+        Runnable runnable2 = () -> {
+            log("Starting Runnable 02.");
+
+            String newDescription = "Detailed description 2 CTM: " + System.currentTimeMillis();
+
+            EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+            entityManager2.getTransaction().begin();
+
+            log("Runnable 02 will load product 2.");
+            Product product = entityManager2.find(
+                    Product.class, 1, LockModeType.PESSIMISTIC_READ);
+//                    Product.class, 1, LockModeType.PESSIMISTIC_WRITE);
+
+            log("Runnable 02 will alter the procut.");
+            product.setDescription(newDescription);
+
+            log("Runnable 02 will wait for 1 second.");
+            wait(1);
+
+            log("Runnable 02 will confirm the transaction.");
+            entityManager2.getTransaction().commit();
+            entityManager2.close();
+
+            log("Finishing Runnable 02.");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+
+        wait(1);
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        EntityManager entityManager3 = entityManagerFactory.createEntityManager();
+        Product product = entityManager3.find(Product.class, 1);
+        entityManager3.close();
+
+        Assert.assertTrue(product.getDescription().startsWith("Detailed description 2"));
+
+        log("Finishing test method.");
+    }
+
 //    @Test // To execute this test it is needed ti comment the version
     public void usePessimistLock_LockModeTypePessimisticWrite() {
         Runnable runnable1 = () -> {
