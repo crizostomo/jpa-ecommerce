ff7ecad6b141d5c52c233a5b5818a24f23ad3518
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-20 10:14:02 -0300

    15.3 | Creating a new database schema

--- src/main/java/com/jpa/ecommerce/util/ExecuteDDL.java
+++ src/main/java/com/jpa/ecommerce/util/ExecuteDDL.java
@@ -12,6 +12,8 @@ public class ExecuteDDL {
     public static void main(String[] args) {
         Map<String, String> properties = new HashMap<>();
 
+        properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost/jpa_store_ecommerce?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC");
+
         properties.put("javax.persistence.schema-generation.database.action",
                 "drop-and-create");
 
