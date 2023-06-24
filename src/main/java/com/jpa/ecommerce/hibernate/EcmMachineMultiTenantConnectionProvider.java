2454aaed399c39358c6901ff4d76f7f062607c27
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-24 12:08:11 -0300

    15.5 | Implementing multitenancy by using machine approach

---
+++ src/main/java/com/jpa/ecommerce/hibernate/EcmMachineMultiTenantConnectionProvider.java
@@ -0,0 +1,111 @@
+package com.jpa.ecommerce.hibernate;
+
+import org.hibernate.engine.config.spi.ConfigurationService;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
+import org.hibernate.hikaricp.internal.HikariCPConnectionProvider;
+import org.hibernate.service.spi.ServiceRegistryAwareService;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
+import org.hibernate.service.spi.Startable;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EcmMachineMultiTenantConnectionProvider implements
+        MultiTenantConnectionProvider, ServiceRegistryAwareService, Startable {
+
+    private Map<String, ConnectionProvider> connectionProviders = null;
+
+    private Map<String, Object> properties = null;
+
+    @Override
+    public Connection getConnection(String tenantIdentifier) throws SQLException {
+        Connection connection = connectionProviders
+                .get(tenantIdentifier)
+                .getConnection();
+
+        return connection;
+    }
+
+    @Override
+    public Connection getAnyConnection() throws SQLException {
+        return getAnyConnectionProvider()
+                .getConnection();
+    }
+
+    @Override
+    public void releaseConnection(String tenantIdentifier, Connection connection) throws SQLException {
+        releaseAnyConnection(connection);
+    }
+
+    @Override
+    public void releaseAnyConnection(Connection connection) throws SQLException {
+        getAnyConnectionProvider().closeConnection(connection);
+    }
+
+    @Override
+    public boolean supportsAggressiveRelease() {
+        return getAnyConnectionProvider().supportsAggressiveRelease();
+    }
+
+    @Override
+    public boolean isUnwrappableAs(Class unwrapType) {
+        return getAnyConnectionProvider().isUnwrappableAs(unwrapType);
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> unwrapType) {
+        return getAnyConnectionProvider().unwrap(unwrapType);
+    }
+
+    @Override
+    public void injectServices(ServiceRegistryImplementor serviceRegistry) {
+        this.properties = serviceRegistry
+                .getService(ConfigurationService.class)
+                .getSettings();
+    }
+
+    @Override
+    public void start() {
+        connectionProviders = new HashMap<>();
+
+        configTenant("jpa_ecommerce",
+                "jdbc:mysql://localhost/jpa_ecommerce?" +
+                        "createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC",
+                "root",
+                "root");
+
+        configTenant("store_ecommerce",
+                "jdbc:mysql://localhost/jpa_store_ecommerce?" +
+                        "createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC",
+                "root",
+                "root");
+
+        this.properties = null;
+    }
+
+    private void configTenant(String tenant, String url, String user, String password) {
+        Map<String, Object> props = new HashMap<>(this.properties);
+
+        props.put("jakarta.persistence.jdbc.url", url);
+        props.put("hibernate.connection.url", url);
+
+        props.put("jakarta.persistence.jdbc.user", user);
+        props.put("hibernate.connection.username", user);
+
+        props.put("jakarta.persistence.jdbc.password", password);
+        props.put("hibernate.connection.password", password);
+
+        HikariCPConnectionProvider cp = new HikariCPConnectionProvider();
+        cp.configure(props);
+
+        this.connectionProviders.put(tenant, cp);
+    }
+
+    private ConnectionProvider getAnyConnectionProvider() {
+        return connectionProviders
+                .values().iterator().next();
+    }
+}
--- src/test/java/com/jpa/ecommerce/multitenant/MultiTenantTest.java
+++ src/test/java/com/jpa/ecommerce/multitenant/MultiTenantTest.java
@@ -10,6 +10,21 @@ import org.junit.Test;
 public class MultiTenantTest extends EntityManagerTest {
 
     @Test
+    public void machineApproach() {
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("jpa_ecommerce");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        Product product1 = entityManager1.find(Product.class, 1);
+        Assert.assertEquals("Kindle", product1.getName());
+        entityManager1.close();
+
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("store_ecommerce");
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        Product product2 = entityManager2.find(Product.class, 1);
+        Assert.assertEquals("Kindle Paperwhite", product2.getName());
+        entityManager2.close();
+    }
+
+    @Test
     public void schemaApproach() {
         EcmCurrentTenantIdentifierResolver.setTenantIdentifier("jpa_ecommerce");
         EntityManager entityManager1 = entityManagerFactory.createEntityManager();
